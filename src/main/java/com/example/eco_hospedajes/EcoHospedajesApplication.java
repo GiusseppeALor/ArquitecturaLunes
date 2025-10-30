96dd3269f935175620ff75ef35ce72f7fc0e780f
GiusseppeALor <112878090+GiusseppeALor@users.noreply.github.com>
2025-10-30 00:09:15 -0500

    java

---
+++ src/main/java/com/example/eco_hospedajes/EcoHospedajesApplication.java
@@ -0,0 +1,13 @@
+package com.example.eco_hospedajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoHospedajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoHospedajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eco_hospedajes/EcoHospedajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eco_hospedajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoHospedajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
