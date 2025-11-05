10c359d51026a31db4e33a418bc2e3b602d22d16
GiusseppeALor <112878090+GiusseppeALor@users.noreply.github.com>
2025-11-05 13:05:14 -0500

    V1.1 login

--- src/main/java/com/example/eco_hospedajes/controller/LoginController.java
+++ src/main/java/com/example/eco_hospedajes/controller/LoginController.java
@@ -4,12 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index"; // Busca templates/index.html
+    @GetMapping("/login")
+    public String mostrarLogin() {
+        return "redirect:/login.html";
     }
-
 }
-
